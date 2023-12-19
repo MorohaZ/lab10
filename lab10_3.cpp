de22df375f4413ebcc4496cfd2b9e40189e43032
MorohaZ <141795069+MorohaZ@users.noreply.github.com>
2023-12-19 15:41:07 +0700

    completed

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<cmath>
+#include<fstream>
+#include<string>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    double sum = 0 ,sum_of_square = 0;
+    string textline ;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+        sum += atof(textline.c_str());
+        sum_of_square += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    
+    
+    cout << "Number of data = "<< count;
+    cout << setprecision(3);
+    cout << "\nMean = " <<sum/count ;
+    cout << "\nStandard deviation = "<< sqrt((sum_of_square/count)-pow(sum/count,2));
+    source.close();
+    return 0 ;
+}
